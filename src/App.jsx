35a8e6b00e1b92651a221881a11f8d7ebd56cdf7
ygregox05 <20609998-ygregox05@users.noreply.replit.com>
2025-02-18 20:50:16 +0000

    User checkpoint: Move logo to `imgs` folder and update App component with styled-components, adding logo and styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,74 @@
-import './App.css'
+import { styled, createGlobalStyle } from 'styled-components'
 
-export default function App() {
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Neutra';
+    src: url(fonts/neutra.otf) format('opentype');
+
+  }
+
+  @font-face {
+    font-family: 'Sf-pro';
+    src: url(fonts/SFPro.ttf) format('truetype');
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    height: 100vh;
+    width: 100vw;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background-color: #1E1F20;
+  }
+`;
+
+const StyledTitle = styled.div`
+  height: 20vh;
+  width: 100vw;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 15px;
+
+  h1{
+    color: #F0F2EF;
+    font-family: 'Neutra'
+  }
+  img{
+    height: 65px;
+    width: 65px;
+  }
+`;
+
+const StyledContainer = styled.div`
+  height: 80vh;
+  width: 100vw;
+
+  background-color: #F0F2EF;
+  border-radius: 30px 30px 0 0;
+`;
+
+function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-  )
+    <>
+      <GlobalStyle />
+      <StyledTitle>
+        <h1>Bem-vindo <br/>de volta!</h1>
+        <img src="imgs/logo.png" alt="Logo" width="100" />
+      </StyledTitle>
+      <StyledContainer>
+      </StyledContainer>
+    </>
+  );
 }
+
+export default App;
