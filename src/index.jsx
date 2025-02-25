12497b28b0836f811e1f2506a8613c9b2cc3ba67
Daniell w. <126434546+Gregox0@users.noreply.github.com>
2025-02-25 08:45:43 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import App from './App.jsx'
-import ConteMais from './pages/ConteMais.jsx';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ConteMais from './pages/ConteMais.jsx'
+import { AuthProvider } from "./context/AuthContext.jsx"
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '/ConteMais',
-		element: <ConteMais />
-	}
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/ConteMais',
+    element: <ConteMais />
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<RouterProvider router={router}/>
-	</StrictMode>,
+  <StrictMode>
+    <AuthProvider> 
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
 )
